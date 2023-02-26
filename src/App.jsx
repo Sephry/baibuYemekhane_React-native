8b09fc82d887a5fa33e37331820d57e2a304ef36
sephry <ismail.kurtf@gmail.com>
2023-02-26 03:55:43 +0300

    qw

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import database from '@react-native-firebase/database';
 import FlatListBasics from './components/FlatListBasic';
 import Header from './components/Header';
-import { FoodWidgetPreviewScreen } from './widget/FoodWidgetPreviewScreen';
 
 function App() {
   const [data, setData] = useState('');
 
-  async function readData() {
-    await database()
+  useEffect(() => {
+    database()
       .ref('food')
       .once('value')
       .then(snapshot => {
         setData(Object.values(snapshot.val()));
       });
-  }
-  readData();
+  }, []);
 
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
-        {/* <FoodWidgetPreviewScreen /> */}
         <Header />
         <FlatListBasics data={data} />
-        
       </View>
     </SafeAreaView>
   );
